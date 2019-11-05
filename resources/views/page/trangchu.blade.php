ec35d6b099d73c0f5faf4e962e9a2d156fc012c9
QUANG <aovanquang@gmail.com>
2019-11-05 15:00:30 +0700

    ok

--- resources/views/page/trangchu.blade.php
+++ resources/views/page/trangchu.blade.php
@@ -100,7 +100,7 @@
                                             </div>
                                             <div class="single-item-caption">
                                                 <a class="add-to-cart pull-left" href="{{route('themgiohang',$sale->id)}}"><i class="fa fa-shopping-cart"></i></a>
-                                                <a class="beta-btn primary" href="{{route('chitietsanpham',$sale->id)}}">Details <i class="fa fa-chevron-right"></i></a>
+                                                <a class="beta-btn primary" href="{{route('chitietsanpham',$sale->id)}}">Chi tiết<i class="fa fa-chevron-right"></i></a>
                                                 <div class="clearfix"></div>
                                             </div>
                                         </div>
